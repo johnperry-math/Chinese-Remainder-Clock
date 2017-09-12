ada432ef4fc70d3b387ba8570548790d4263747f
John Perry <john.perry@usm.edu>
2017-09-12 17:18:36 -0500

    Added Italian translation by Antonella Perucca. A few other minor tweaks.

--- app/src/main/java/name/cantanima/chineseremainderclock/CRC_Webview.java
+++ app/src/main/java/name/cantanima/chineseremainderclock/CRC_Webview.java
@@ -7,15 +7,24 @@ import android.view.View;
 import android.webkit.WebView;
 import android.app.Activity;
 
+import java.util.Locale;
+
+import static java.util.Locale.ITALIAN;
+
 /**
  * Created by cantanima on 4/26/17.
  */
 
 public class CRC_Webview extends WebView {
 
-    public CRC_Webview(Context context, AttributeSet attrs) {
-        super(context, attrs);
-        loadUrl("file:///android_asset/Help.html");
-    }
+  public CRC_Webview(Context context, AttributeSet attrs) {
+    super(context, attrs);
+
+    String language = Locale.getDefault().getLanguage();
+    if (language.equals(ITALIAN.getLanguage()))
+      loadUrl("file:///android_asset/Help-it.html");
+    else
+      loadUrl("file:///android_asset/Help.html");
+  }
 
 }
